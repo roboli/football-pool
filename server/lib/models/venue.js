c80ede9f243cffdf8853326d7c3c3b6c0a148ce6
Roberto Oliveros <robertooliveros@mac.com>
2014-03-06 10:11:41 -0600

    Validating name property.

--- server/lib/models/venue.js
+++ server/lib/models/venue.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var VenueSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   location: String,
   capacity: Number
 });
