630b05277efee17eea72de958daeebcfa9177ce3
Roberto Oliveros <robertooliveros@mac.com>
2014-03-06 10:11:15 -0600

    Validating name property.

--- server/test/models/venue.js
+++ server/test/models/venue.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect;
 var db = require('../../lib/db');
 var Venue = require('../../lib/models/venue.js');
+var mongoose = require('mongoose');
 
 describe('Venue model', function() {
   describe('test validations', function() {
@@ -8,6 +9,22 @@ describe('Venue model', function() {
       var venue = new Venue({ name: 'Maracana', location: 'Rio de Janeiro', capacity: 99000 });
       venue.save(function(err) {
 	expect(err).to.not.exist;
+	done();
+      });
+    });
+
+    it('should return error for saving without name', function(done) {
+      var venue = new Venue({ location: 'Rio de Janeiro', capacity: 99000 });
+      venue.save(function(err) {
+	expect(err).to.exist;
+	done();
+      });
+    });
+
+    it('should return error for saving with empty name', function(done) {
+      var venue = new Venue({ name: '', location: 'Rio de Janeiro', capacity: 99000 });
+      venue.save(function(err) {
+	expect(err).to.exist;
 	done();
       });
     });
