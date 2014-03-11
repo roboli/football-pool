36e015a8177d04a0eae04ff3582a9122d8dfd974
Roberto Oliveros <robertooliveros@mac.com>
2014-03-11 12:52:18 -0600

    Reorganizing beforeEach functions for reusing the mocks for other tests.

--- client/test/unit/app/admin/admin.spec.js
+++ client/test/unit/app/admin/admin.spec.js
@@ -1,24 +1,25 @@
 describe('Admin', function() {
-
+  var scope, $httpBackend;
+  
   beforeEach(module('admin'));
 
-  describe('TournamentCtrl', function() {
-    var scope, ctrl, $httpBackend;
-    var tournament = {
-      "name": "FIFA World Cup 2014",
-      "startDate": "2014-06-01T13:00:00-03:00",
-      "endDate": "2014-07-01T19:00:00-03:00"
-    };
+  beforeEach(inject(function(_$httpBackend_, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    scope = $rootScope.$new();
+  }));
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-      $httpBackend = _$httpBackend_;
+  describe('TournamentCtrl', function() {
+
+    it('should create tournament model', inject(function($controller) {
+      var tournament = {
+	"name": "FIFA World Cup 2014",
+	"startDate": "2014-06-01T13:00:00-03:00",
+	"endDate": "2014-07-01T19:00:00-03:00"
+      };
+
+      $controller('TournamentCtrl', { $scope: scope });
       $httpBackend.expectGET('/tournament').respond(tournament);
 
-      scope = $rootScope.$new();
-      ctrl = $controller('TournamentCtrl', { $scope: scope });
-    }));//
-
-    it('should create tournament model from xhr', function() {
       expect(scope.tournament.name).toBeUndefined();
       expect(scope.tournament.startDate).toBeUndefined();
       expect(scope.tournament.endDate).toBeUndefined();
@@ -28,6 +29,6 @@ describe('Admin', function() {
       expect(scope.tournament.name).toEqual(tournament.name);
       expect(scope.tournament.startDate).toEqual(tournament.startDate);
       expect(scope.tournament.endDate).toEqual(tournament.endDate);
-    });
+    }));
   });
 });
