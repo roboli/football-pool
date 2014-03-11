ddc2b446e51d73b4cce1ac068c7a411bb9afba4b
Roberto Oliveros <robertooliveros@mac.com>
2014-03-11 09:44:31 -0600

    Installing grunt-contrib-watch.

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
 
   grunt.initConfig({
@@ -15,10 +16,23 @@ module.exports = function(grunt) {
     },
     karma: {
       unit: {
-	configFile: 'test/config/karma.conf.js'
+	configFile: 'test/config/karma.conf.js',
+	autoWatch: false,
+	background: true
+      }
+    },
+    watch: {
+      gruntfile: {
+	files: ['gruntfile.js'],
+	tasks: ['jshint']
+      },
+      test: {
+	files: ['src/**/*.js', 'test/**/*.js'],
+	tasks: ['jshint', 'karma:unit:run']
       }
     }
   });
   
   grunt.registerTask('test', [ 'jshint:test', 'karma' ]);
+  grunt.registerTask('test-watch', [ 'jshint:test', 'karma:unit:start', 'watch' ]);
 };
