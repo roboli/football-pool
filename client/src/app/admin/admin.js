af87f2a6f963765d57fd4f086695f01f93c80d8a
Roberto Oliveros <robertooliveros@mac.com>
2014-03-14 04:59:51 -0600

    Implementing routes.

--- client/src/app/admin/admin.js
+++ client/src/app/admin/admin.js
@@ -1,4 +1,18 @@
-angular.module('admin', ['resources.tournament', 'resources.venue'])
+angular.module('admin', ['ngRoute', 'resources.tournament', 'resources.venue'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/templates/admin.tpl.html',
+    })
+    .when('/venues', {
+      templateUrl: '/templates/venues-list.tpl.html',
+      controller: 'VenueListCtrl'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+}])
 
 .controller('TournamentCtrl', ['$scope', 'Tournament', function($scope, Tournament) {
   $scope.tournament = Tournament.get();
