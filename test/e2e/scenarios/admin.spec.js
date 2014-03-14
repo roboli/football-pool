cc5d18c9137381e5dd6e7df0f78ea11ed657c611
Roberto Oliveros <robertooliveros@mac.com>
2014-03-14 05:00:24 -0600

    Testing navigation to the venues list page.

--- test/e2e/scenarios/admin.spec.js
+++ test/e2e/scenarios/admin.spec.js
@@ -1,10 +1,17 @@
 describe('Admin', function() {
 
   beforeEach(function() {
+    browser.ignoreSynchronization = true;
     browser.get('index.html');
   });
 
   it('should show tournament header', function() {
     expect(element(by.binding('tournament.name')).getText()).toBe('FIFA World Cup 2014');
+    expect(element(by.tagName('p')).getText()).toBe('Welcome to the administration app!');
+  });
+
+  it('should show venues list', function() {
+    element(by.css("a[href='#/venues']")).click();
+    expect(element(by.tagName('p')).getText()).toBe('Here goes the venues...');
   });
 });
