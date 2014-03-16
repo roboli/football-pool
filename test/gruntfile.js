4d9adefb494ba47e54618a043564009fcb23cb1f
Roberto Oliveros <robertooliveros@mac.com>
2014-03-16 05:40:15 -0600

    Using grunt-protractor-runner package.

--- test/gruntfile.js
+++ test/gruntfile.js
@@ -1,40 +1,28 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-protractor-runner');
 
   grunt.initConfig({
-    env: {
-      test: { NODE_ENV: 'TEST' },
-    },
     jshint: {
       test: {
-	src: ['lib/**/*.js', 'test/**/*.js', 'config/*.json'],
+	src: ['e2e/**/*.js', './*.json'],
       },
       options: {
 	expr: true,
 	'-W099': true // supress spaces and tabs warning
       }
     },
-    cafemocha: {
-      test: {
-        src: 'test/**/*.js',
-        options: {
-          ui: 'bdd',
-          reporter: 'spec'
-	}
-      }
-    },
-    watch: {
-      gruntfile: {
-	files: ['gruntfile.js'],
-	tasks: ['jshint']
+    protractor: {
+      options: {
+	configFile: "./protractor-conf.js",
+	keepAlive: false, 
+	noColor: false, 
       },
       test: {
-	files: ['lib/**/*.js', 'test/**/*.js', 'config/*.json'],
-	tasks: ['env', 'jshint', 'cafemocha']
       }
     }
   });
   
-  grunt.registerTask('test', [ 'env:test', 'jshint:test', 'cafemocha:test' ]);
+  grunt.registerTask('test', [ 'jshint:test', 'protractor' ]);
 };
