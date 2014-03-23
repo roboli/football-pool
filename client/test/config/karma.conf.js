7d54f63176c6d9861a2568cf96a8f65a96906106
Roberto Oliveros <robertooliveros@mac.com>
2014-03-22 19:21:09 -0600

    Angular bootstrap installed.

--- client/test/config/karma.conf.js
+++ client/test/config/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-bootstrap/ui-bootstrap.js',
       'src/**/*.js',
       'test/**/*.js'
     ],
